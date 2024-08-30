81aed0cbe785e3b51f45ce7ee12065a453beaa12
Luis Gimenez <luisgimenezdev@gmail.com>
2024-08-30 17:42:28 -0400

    hide sensitive info

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrPXtYcSZ8q2REkZVniiGnm1DZtdxOEII",
-  authDomain: "st-trader-np.firebaseapp.com",
-  projectId: "st-trader-np",
-  storageBucket: "st-trader-np.appspot.com",
-  messagingSenderId: "708256667332",
-  appId: "1:708256667332:web:f7bc3f00018d8587609b91",
-  measurementId: "G-HVLYDMX2L5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
